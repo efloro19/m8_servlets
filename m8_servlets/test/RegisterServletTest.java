4f8c6094a456fb98be48d14a382ceb5e968c03a8
hidro <hidro@192.168.1.113>
2020-04-27 21:44:59 +0200

    Limpieza, retoques y añado tests

---
+++ m8_servlets/test/RegisterServletTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RegisterServletTest {
+
+	@Test
+	public void testRegisterServlet() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testDoPost() {
+		fail("Not yet implemented");
+	}
+
+}
